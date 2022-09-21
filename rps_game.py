e1e7efef1a95dd761b637ec8c08767cff9754b27
Entility <83473910+Entility@users.noreply.github.com>
2022-09-21 09:07:22 +0300

    Add files via upload

---
+++ rps_game.py
@@ -0,0 +1,12 @@
+
+human_turn='rock'
+computer_turn=choices='scissors'
+if human_turn == 'rock' and computer_turn =='scissors':
+    print('Human wins!')
+elif human_turn == 'paper' and computer_turn =='rock':
+    print('Human wins!')
+
+elif human_turn == 'scissors' and computer_turn =='paper':
+    print('Human wins!')
+
+
